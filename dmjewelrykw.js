2a13340f7a1c1b81cc1721bd28f027f245d6287b
KWilliams95 <86546302+KWilliams95@users.noreply.github.com>
2021-07-20 22:44:51 -0400

    Update dmjewelrykw.js

--- dmjewelrykw.js
+++ dmjewelrykw.js
@@ -1,10 +1,19 @@
-function toggleDarkMode(){
-  let background = document.getElementById("body");
+(function() {
+   var button = document.querySelector("#navigation-button");
+   var nav = document.querySelector("#navigation-items");
+   button.addEventListener("click", function(e) {
+     e.preventDefault();
+     nav.classList.contains("navigation-items--visible") ? nav.classList.remove("navigation-items--visible") : nav.classList.add("navigation-items--visible");
+   });
+ })();
 
-  if (background.style.background =='black'){
-    background.style.background = 'Lavender'
-  }
-  else {
-    background.style.background ='black';
-  }
+function toggleDarkMode(){
+ let background = document.getElementById("body");
+
+ if (background.style.background =='black'){
+   background.style.background = 'Lavender'
+ }
+ else {
+   background.style.background ='black';
+ }
 }
